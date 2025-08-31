085ef9e0e8df01eb01221b9c42374c1dc8c1f231
Morshed <morshedmto@gamil.com>
2025-08-31 21:15:25 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { ThemeToggle } from "@/components/ui/themeToggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <ThemeToggle/>
+   </div>
+  );
+}
