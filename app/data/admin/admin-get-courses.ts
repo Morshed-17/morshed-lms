928d1415ff462b8946eb90fc31aa041f958cc046
Morshed <morshedmto@gamil.com>
2025-09-03 16:57:07 +0600

    Course display in admin

--- app/data/admin/admin-get-courses.ts
+++ app/data/admin/admin-get-courses.ts
@@ -1,18 +1,27 @@
 import { prisma } from "@/lib/db";
 import { requireAdmin } from "./require-admin";
 
-export async function adminGetCourses (){
-    await requireAdmin()
+export async function adminGetCourses() {
+  await requireAdmin();
 
-    const data = await prisma.course.findMany({
-        select: {
-            id: true,
-            title: true,
-            smallDescription: true,
-            duration: true,
-            
+  const data = await prisma.course.findMany({
+    orderBy: {
+      cretedAt: "desc", // Newest results first
+    },
+    select: {
+      id: true,
+      title: true,
+      smallDescription: true,
+      duration: true,
+      level: true,
+      status: true,
+      price: true,
+      fileKey: true,
+      slug: true,
+    },
+  });
 
-        }
-    })
-
+  return data;
 }
+
+export type AdminCourseType = Awaited<ReturnType<typeof adminGetCourses>>[0];
